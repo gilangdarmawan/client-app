291e5e32cb90c5d0ee47c2b3ba7b961adf02dc96
gilangdarmawan <jobs.gilangdarmawan@gmail.com>
2019-06-18 02:59:57 +0700

    commit client-app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ListDoctorsComponent } from './list-doctors/list-doctors.component';
+import { AddDoctorComponent } from './add-doctor/add-doctor.component';
+import { EditDoctorComponent } from './edit-doctor/edit-doctor.component';
+import { ViewChartComponent } from './view-chart/view-chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'add-doctor', component: AddDoctorComponent },
+    { path: 'edit-doctor', component: EditDoctorComponent},
+    { path: 'doctor-list', component: ListDoctorsComponent},
+    { path: '', component: ViewChartComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
